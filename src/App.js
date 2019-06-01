5f3ee3be2457e0b297d172b151b502609a992dfc
James Youngblood <james@youngbloods.org>
2019-06-01 16:29:05 -0600

    Add React Router DOM

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return <div className="App">
-    test
-  </div>;
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <main className='app'>
+          <h1>Lotus Forums</h1>
+        </main>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
